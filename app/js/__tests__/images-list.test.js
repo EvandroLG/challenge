1014d3bb49a65673d74952414c50fc2ab7e1a1ed
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-05-01 16:49:02 +0100

    add some tests to images list module

---
+++ app/js/__tests__/images-list.test.js
@@ -0,0 +1,54 @@
+import apiMock from '../__mocks__/api';
+import ImagesList from '../images-list';
+import http from '../http';
+
+describe('images-list', () => {
+  http.get = jest.fn(() => {
+    return new Promise(resolve => resolve(apiMock));
+  });
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <ul id="js-list">
+      </ul>
+    `;
+  });
+
+  it('should update list html with the datas provided by the api', async () => {
+    const imagesList = new ImagesList();
+    await imagesList.update('cat');
+
+    expect(http.get).toHaveBeenCalled();
+    expect(
+      document.getElementById('js-list').innerHTML
+    ).toMatchSnapshot();
+  });
+
+  it('should request and display more images', async () => {
+    const imagesList = new ImagesList();
+    await imagesList._updateNextPage();
+    await imagesList._updateNextPage();
+
+    expect(imagesList.page).toEqual(2);
+    expect(http.get).toHaveBeenCalled();
+    expect(
+      document.getElementById('js-list').innerHTML
+    ).toMatchSnapshot();
+  });
+
+  it('should not display more images when request returns empty data', async () => {
+    http.get = jest.fn(() => {
+      return new Promise(resolve => resolve({
+        data: [],
+      }));
+    });
+
+    const imagesList = new ImagesList();
+    await imagesList._updateNextPage();
+
+    expect(imagesList.page).toEqual(1);
+    expect(
+      document.getElementById('js-list').querySelectorAll('li').length
+    ).toEqual(0);
+  });
+});
