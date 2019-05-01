d9af14cc0ceec928a97f05b43c94fd848966e2b7
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-05-01 18:33:17 +0100

    update api mock

--- app/js/__mocks__/api.js
+++ app/js/__mocks__/api.js
@@ -2,7 +2,7 @@ export default {
   data: [
     {
       images: {
-        original: {
+        fixed_height: {
           url: 'https://media2.giphy.com/media/3o7aTIrTw1ckKbqra8/giphy.gif',
           width: '360',
           height: '270',
@@ -12,7 +12,7 @@ export default {
 
     {
       images: {
-        original: {
+        fixed_height: {
           url: 'https://media0.giphy.com/media/3o7aTsNEoU0k75RWKI/giphy.gif',
           width: '480',
           height: '270',
@@ -22,7 +22,7 @@ export default {
 
     {
       images: {
-        original: {
+        fixed_height: {
           url: 'https://media2.giphy.com/media/3o7aTIrTw1ckKbqra8/giphy.gif',
           width: '360',
           height: '270',
@@ -32,7 +32,7 @@ export default {
 
     {
       images: {
-        original: {
+        fixed_height: {
           url: 'https://media0.giphy.com/media/3o7aTsNEoU0k75RWKI/giphy.gif',
           width: '480',
           height: '270',
@@ -42,7 +42,7 @@ export default {
 
     {
       images: {
-        original: {
+        fixed_height: {
           url: 'https://media2.giphy.com/media/3o7aTIrTw1ckKbqra8/giphy.gif',
           width: '360',
           height: '270',
