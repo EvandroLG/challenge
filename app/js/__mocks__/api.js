51a9288dd4d9287da4f934df3c1d5e0cc98a04fa
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-05-01 16:49:26 +0100

    add api mock

---
+++ app/js/__mocks__/api.js
@@ -0,0 +1,53 @@
+export default {
+  data: [
+    {
+      images: {
+        original: {
+          url: 'https://media2.giphy.com/media/3o7aTIrTw1ckKbqra8/giphy.gif',
+          width: '360',
+          height: '270',
+        },
+      },
+    },
+
+    {
+      images: {
+        original: {
+          url: 'https://media0.giphy.com/media/3o7aTsNEoU0k75RWKI/giphy.gif',
+          width: '480',
+          height: '270',
+        },
+      },
+    },
+
+    {
+      images: {
+        original: {
+          url: 'https://media2.giphy.com/media/3o7aTIrTw1ckKbqra8/giphy.gif',
+          width: '360',
+          height: '270',
+        },
+      },
+    },
+
+    {
+      images: {
+        original: {
+          url: 'https://media0.giphy.com/media/3o7aTsNEoU0k75RWKI/giphy.gif',
+          width: '480',
+          height: '270',
+        },
+      },
+    },
+
+    {
+      images: {
+        original: {
+          url: 'https://media2.giphy.com/media/3o7aTIrTw1ckKbqra8/giphy.gif',
+          width: '360',
+          height: '270',
+        },
+      },
+    },
+  ],
+};
