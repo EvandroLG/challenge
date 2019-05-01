a243cbb2d81a4270f40fc8d2b5e6664a7395b51a
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-05-01 17:37:35 +0100

    update test of index module

--- app/js/__tests__/index.test.js
+++ app/js/__tests__/index.test.js
@@ -1,15 +1,18 @@
 import ImagesList from '../images-list';
 import Search from '../search';
+import Buttons from '../buttons';
 import '../index';
 
 describe('index', () => {
-  it('should initialize the ImageList and Search components', () => {
+  it('should initialize the ImageList, Search and Buttons components', () => {
     ImagesList.prototype.updateOnScroll = jest.fn();
     Search.prototype.initialize = jest.fn();
+    Buttons.prototype.initialize = jest.fn();
 
     document.dispatchEvent(new Event('DOMContentLoaded'));
 
     expect(ImagesList.prototype.updateOnScroll).toHaveBeenCalled();
     expect(Search.prototype.initialize).toHaveBeenCalled();
+    expect(Buttons.prototype.initialize).toHaveBeenCalled();
   });
 });
