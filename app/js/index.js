f269c0fe1716690d8306046f7f7ffb906ea4d7cf
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-05-01 17:31:14 +0100

    update index.js to initialize buttons module

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,11 @@
 import ImageList from './images-list.js';
 import Search from './search.js';
+import Buttons from './buttons.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const imageList = new ImageList();
   imageList.updateOnScroll();
 
   new Search(imageList).initialize();
+  new Buttons().initialize();
 }, false);
