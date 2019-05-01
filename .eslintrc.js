fa382c2e4065cc010f463196b05fb58934426a4c
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-05-01 16:00:50 +0100

    create basic structure to start the project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'airbnb-base',
+
+  env: {
+    browser: true,
+  },
+
+  rules: {
+    'import/extensions': 'off',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-destructuring': 'off',
+  },
+};
